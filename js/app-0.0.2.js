a2b57333c34099658df4550e4f0236a436df2ce5
Luke Davis <lukefable@gmail.com>
2014-09-21 15:47:56 +0000

    removed unnecessary methods

--- js/app-0.0.2.js
+++ js/app-0.0.2.js
@@ -14,7 +14,7 @@ var app={m:{},v:{},c:{}};
 
 app.c.init=function(){
 	app.m.password=false;
-	app.m.metadata={"name":"Signum Velox","version":"0.0.2"};
+	app.m.metadata={"name":"There Will be Rainbows","version":"0.0.2"};
 	var b=app.c.bounds();
 	app.m.genome={};
 	app.m.genome.r=[];
@@ -41,7 +41,7 @@ app.c.listeners=function(){
 		for (var i=0;i<1;i++){
 			var iconWidth=$("input[name=size]:checked").val();
 			iconWidth=parseInt(iconWidth);
-			app.v.icon("div#icons",iconWidth);
+			app.v.tower("div#icons",iconWidth);
 			//console.log(iconWidth);
 		}
 	});
@@ -93,8 +93,7 @@ app.c.breed=function(n){
 
 app.v.layout=function(){
 	var d="";
-	d+="<div class='wrapper'><input type='text' value='Velox'></div>";
-	d+="<p>click more for a new icon. when you find one you like, click it to save. To change the letter, type something new in the box above.</p>";
+	d+="<h1>There Will be Rainbows</h1>";
 	d+="<div id='radios'><form action=''>";
 
 		d+="<table><tr>";
@@ -124,8 +123,7 @@ app.v.init=function(){
 };
 
 app.v.tower=function(target,width){
-	app.m.text=$("input[type=text]").val().split("")[0];
-
+	
 	var width=width || 144;
 	var height=width;
 	var target=target || "body";
@@ -164,20 +162,12 @@ app.v.tower=function(target,width){
 	var ctx=c.getContext("2d");
 	
 	app.v.radial(ctx,opts);
-	davis.maybe(1,3,function(){	
-		ctx.clearRect(0, 0, width, height);
-		app.v.textIcon(ctx,opts);
-	})
-
-	//app.v.textIcon(ctx,width,height);
-	//app.v.bilateral(ctx,width,height);
-	//app.v.radial(ctx,width,height);
 
 };
 
 
 app.v.icon=function(target,width){
-	app.m.text=$("input[type=text]").val().split("")[0];
+	//app.m.text=$("input[type=text]").val().split("")[0];
 
 	var width=width || 144;
 	var height=width;
@@ -226,144 +216,6 @@ app.v.icon=function(target,width){
 	//app.v.bilateral(ctx,width,height);
 	//app.v.radial(ctx,width,height);
 
-};
-
-
-app.v.textIcon=function(ctx,opts){
-	var width=opts.width || 512;
-	var height=opts.height || 512;
-	var color=opts.color || davis.randomColor();
-	var ngonN=opts.ngonN || 3+davis.random(6);
-	var ngonR=opts.ngonR || width/4+davis.random(width/4);
-
-	ctx.beginPath();
-	var gradient=ctx.createLinearGradient(0,0,0,height);
-	gradient.addColorStop(0,color);
-	gradient.addColorStop(1,davis.pick([color,davis.randomColor(),"#000","#fff",davis.alpha(color,0)]));
-	ctx.fillStyle=gradient;
-	ctx.fillRect(0,0,width,height);
-	
-	
-	for (var j=0;j<davis.random(4);j++){
-		var r=davis.random(width/2);
-		var rf=davis.random(width/2);
-		var color=davis.randomColor();
-		davis.maybe(2,3,function(){color=davis.randomColor("grey");});
-		var lw=1+davis.random(width/10);
-		var steps=1+davis.random(30);
-		var incrementMod=davis.random(3)*90;
-		var n=davis.random(6);
-		var rotation=90*davis.random(3);
-		var fadeColor=davis.pick(["rgba(0,0,0,0)","rgba(255,255,255,0)"]);
-		var fadeRadius=Math.random();
-		davis.maybe(1,5,function(){fadeRadius=0;});
-		davis.maybe(1,5,function(){fadeRadius=false;});
-		for (var i=0;i<steps;i++){
-			var increment=i*360/steps;
-			var x=geo.getPoint(width/2,height/2,rf,increment+incrementMod).x2;
-			var y=geo.getPoint(width/2,height/2,rf,increment+incrementMod).y2;
-
-			app.v.circle({
-				n:n,
-				gradient:true,
-				context:ctx,
-				x:x,
-				y:y,
-				r:r,
-				rotation:rotation,
-				lineWidth:lw,
-				color:color,
-				fadeColor:fadeColor,
-				fadeRadius:fadeRadius
-			});	
-		}
-	}
-
-	pattern=ctx.createPattern(ctx.canvas,"no-repeat");
-	ctx.clearRect(0, 0, width, height);
-
-
-	app.v.ngon({
-		n:ngonN,
-		gradient:true,
-		context:ctx,
-		x:width/2,
-		y:height/2,
-		r:ngonR,
-		fill:pattern,
-		lineWidth:davis.random(width/15)
-	});	
-
-	var x=width/2;
-	var y=height/2;
-	var fonts=[
-		"Verdana, Geneva, sans-serif",
-		"Courier, monospace",
-		"Monaco, monospace",
-		"Helvetica, sans-serif",
-		"Impact, Charcoal, sans-serif",
-		"cursive, sans-serif",
-		"'Arial Black', Gadget, sans-serif",
-		"'Times New Roman', Times, serif",
-		"'Palatino Linotype', 'Book Antiqua', Palatino, serif"
-	];
-	var text=app.m.text;
-	if (typeof text!="string"){
-		text=" ";
-	}
-	davis.maybe(1,4,function(){text=text.toUpperCase();});
-	davis.maybe(1,4,function(){text=text.toLowerCase();});
-	ctx.textAlign = "center";
-	ctx.textBaseline = "middle";
-	ctx.font="900 "+((width/4)+davis.random(width/4))+"px "+davis.pick(fonts);
-	//ctx.fillStyle=pattern;
-	ctx.fillStyle="#fff";
-	ctx.fillText(text,x,y);
-};
-
-
-app.v.bilateral=function(ctx,width,height){
-	var color=davis.randomColor();
-	var strokeStyle=davis.alpha(davis.randomColor("grey"),(0.5+(Math.random()/2)));
-	var fillStyle=davis.alpha(color,Math.random());
-	for (var i=0;i<4;i++){
-		davis.maybe(1,2,function(){
-
-			var lineWidth=1+davis.random(width/100);
-			var r=davis.random(width/3);
-
-			var x=r+lineWidth+davis.random((width/2)-r-lineWidth);
-			var y=r+lineWidth+davis.random((height)-(2*(r+lineWidth)));
-			ctx.beginPath();
-			ctx.strokeStyle=strokeStyle;
-			ctx.arc(x,y,r,0,2*Math.PI);
-			ctx.lineWidth=lineWidth;
-			ctx.fillStyle=fillStyle;
-			ctx.fill();
-			ctx.stroke();
-			ctx.beginPath();
-			x=width/2+(width/2)-x;
-			ctx.arc(x,y,r,0,2*Math.PI);
-			ctx.fillStyle=fillStyle;
-			ctx.fill();
-			ctx.stroke();
-		});
-	}
-	for (var i=0;i<3;i++){
-		davis.maybe(1,2,function(){
-			var lineWidth=1+davis.random(width/100);
-			var r=davis.random(width/3);
-			var x=width/2;
-			var y=r+lineWidth+davis.random((height)-(2*(r+lineWidth)));
-			ctx.beginPath();
-			ctx.strokeStyle=strokeStyle;
-			ctx.arc(x,y,r,0,2*Math.PI);
-			ctx.lineWidth=lineWidth;
-			ctx.fillStyle=fillStyle;
-			davis.maybe(1,3,function(){ctx.fill();});
-			ctx.stroke();
-		})
-	}
 };
 
 app.v.radial=function(ctx,opts){
